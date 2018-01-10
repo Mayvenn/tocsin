202f04852b4adc4e34fe9e33dd741a2c5d8b5b31
Greg Winn <gregw@mayvenn.com>
2018-01-10 14:00:15 -0800

    Version 0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tocsin "0.1.3-SNAPSHOT"
+(defproject tocsin "0.1.3"
   :description "Sends exceptions"
   :url "https://github.com/Mayvenn/tocsin"
   :license {:name "Eclipse Public License"
