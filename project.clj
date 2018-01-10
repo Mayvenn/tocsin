080611182608cebda030050f26e24523fe8a4bc7
Greg Winn <gregw@mayvenn.com>
2018-01-10 14:00:04 -0800

    Bump dependencies

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [cheshire "5.5.0"]
+                 [cheshire "5.8.0"]
                  [clj-bugsnag "0.2.9"]]
   :profiles
   {:dev {:source-paths ["dev"]
-         :dependencies [[diff-eq "0.1.0"]
-                        [org.clojure/tools.namespace "0.2.9"]]
+         :dependencies [[diff-eq "0.2.5"]
+                        [org.clojure/tools.namespace "0.2.11"]]
          :injections [(require 'diff-eq.core)
                       (diff-eq.core/diff!)]}})
