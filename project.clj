a8ba821e182dadc6673f3d701da43af0b8af2e3f
Justin Watt <justinw@mayvenn.com>
2018-05-09 14:10:18 -0700

    Bump clojure to 1.9
    Co-authored-by: Jeff Hui <jeff@mayvenn.com>

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/Mayvenn/tocsin"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [cheshire "5.8.0"]
                  [clj-bugsnag "0.2.9"]]
   :profiles
